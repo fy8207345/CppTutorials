e0b41b71a211af14960f27bcee01fbfbfa6feeab
Feng Yan <2316696377@qq.com>
2022-02-21 13:47:12 +0800

    input

--- CppTutorials/3.1Statements/main.cpp
+++ CppTutorials/3.1Statements/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
-int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+int addNumbers(int first, int second)
+{
+    return first + second;
+}
+
+int main(int argc, char** argv)
+{
+    int firstNumber = 12;
+    int secondNumber = 9;
+
+    int first = {3};
+    int second = {5};
+
+    int sum1 = addNumbers(first, second);
+    std::cout << "the sum of the numbers is : " << sum1 << std::endl;
+
+    int sum = addNumbers(firstNumber, secondNumber);
+    std::cout << "the sum of the two numbers is : " << sum << std::endl;
     return 0;
 }
---
+++ CppTutorials/3.2InputAndOutput/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello!" << std::endl;
+    //Error
+    std::cerr << "Error!" << std::endl;
+    //Log Message
+    std::clog << "Log!" << std::endl;
+
+    int age;
+    std::string name;
+
+    std::cout << "Please type in your last name:" << std::endl;
+    std::cin >> name;
+
+    std::cout << "Please type in your age: " << std::endl;
+    std::cin >> age;
+
+    std::cout << "Hello " << name << "! Your are " << age << " years old!" << std::endl;
+
+    int age1;
+    std::string full_name;
+
+    std::cout << "Please type in your full name:" << std::endl;
+    std::getline(std::cin, full_name);
+
+    std::cout << "Type in your age : " << std::endl;
+    std::cin >> age1;
+    std::cout << "Hello " << full_name << "! Your are " << age1 << " years old!" << std::endl;
+    return 0;
+}
